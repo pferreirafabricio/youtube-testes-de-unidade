9f92359b58f0af147426aad511731cd3b36a92bc
Fabrício Pinto Ferreira <pferreirafabricio@gmail.com>
2021-02-18 14:40:16 -0300

    test: cria teste para a classe Pessoa

---
+++ tests/PessoaTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class PessoaTest extends TestCase
+{
+    public function testPessoaVaiTerIdade()
+    {
+        $pessoa = new Pessoa('Fabrício', new DateTimeImmutable('2001-09-18'));
+
+        $this->assertEquals($pessoa->idade(), 19);
+    }
+}
